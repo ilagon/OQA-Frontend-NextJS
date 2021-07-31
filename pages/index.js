a29226965de1646831bf9c758729dc442d2d7149
Adrian <lorenzyadrian@gmail.com>
2021-08-01 01:24:52 +0800

    Edit of login page

--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,18 @@ export default function Home() {
 
   const [userName, setUserName] = useState();
   const [password, setPassword] = useState();
-  const [error, setError] = useState(false);
+  const [error, setError] = useState();
 
   const handleSubmit = () => {
-    
   }
   return (
     <div className='GlobalCenterContainer'>
       <form onSubmit={handleSubmit} className={style.loginContainer}>
-        <h1>Login</h1>
         <input type='text' placeholder="Username:"/><br/>
         <input type='password' placeholder="Password:"/><br/>
         <button type="submit">Login</button>
-      </form>
       {error ? <ErrorBox ErrorMessage="Invalid username or password"/> : null}
+      </form>
     </div>
   );
 }
